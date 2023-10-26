b1a2512369d7019cdbd22d68bb1f98ac6750f72e
Abreham <abreham.gmedhin12@gmail.com>
2023-10-26 19:31:43 +0300

    added review functionality

--- Car_rental_service/settings.py
+++ Car_rental_service/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import accounts.apps
 import rental.apps
+import review.apps
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,7 +45,7 @@ INSTALLED_APPS = [
     'fleet.apps.FleetConfig',
     'accounts.apps.AccountsConfig',
     'rental.apps.RentalConfig',
-    'celery'
+    'review.apps.ReviewConfig'
 ]
 
 MIDDLEWARE = [
